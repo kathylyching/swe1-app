28e8598e35c81e61f095344743a6a5e46e747207
Kathy Ching <kathyching@10-18-127-129.dynapool.wireless.nyu.edu>
2021-10-06 14:49:17 -0400

    stable version

--- polls/admin.py
+++ polls/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
+from .models import Choice, Question
 
-from .models import Question
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
 
 admin.site.register(Question)
+admin.site.register(Choice)
