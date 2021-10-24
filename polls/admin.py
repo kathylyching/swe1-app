4eec7f48665e2047f607fba58d0b44cf878f29b7
Kathy Ching <kc3191@nyu.edu>
2021-10-23 23:35:35 -0400

    Style Changes

--- polls/admin.py
+++ polls/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from .models import Choice, Question
 
+
 class ChoiceInline(admin.TabularInline):
     model = Choice
     extra = 3
+
 
 admin.site.register(Question)
 admin.site.register(Choice)
--- polls/urls.py
+++ polls/urls.py
@@ -1,8 +1,5 @@
 from django.urls import path
-
 from . import views
-import polls
-
 
 app_name = 'polls'
 urlpatterns = [
