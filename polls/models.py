62e6f4c343406e3618677fac868eff548668507f
Kathy Ching <kc3191@nyu.edu>
2021-10-24 16:34:01 -0400

    Update models.py

--- polls/models.py
+++ polls/models.py
@@ -5,6 +5,7 @@ from django.utils import timezone
 
 
 class Question(models.Model):
+    # ...
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField("date published")
 
@@ -16,6 +17,7 @@ class Question(models.Model):
 
 
 class Choice(models.Model):
+    # ...
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
